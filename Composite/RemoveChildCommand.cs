08bed15ed89cce39eee15609f8187ab535a6f448
Климчук Анна Богданівна <ipz242_kab@student.ztu.edu.ua>
2026-05-02 18:43:31 +0300

    Add commands for adding and removing elements

---
+++ Composite/RemoveChildCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatternsLab.Composite
+{
+	class RemoveChildCommand : ICommand
+	{
+		private LightElementNode parent;
+		private LightNode child;
+
+		public RemoveChildCommand(LightElementNode parent, LightNode child)
+		{
+			this.parent = parent;
+			this.child = child;
+		}
+
+		public void Execute()
+		{
+			parent.GetChildren().Remove(child);
+		}
+	}
+}
